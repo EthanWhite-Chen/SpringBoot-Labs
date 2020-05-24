3bd51bfcefafc62daa4a880691301a71986c80ac
Ethan <48879998+EthanWhite-Chen@users.noreply.github.com>
2020-05-24 21:59:22 +0800

    新增redis-key 的学习

---
+++ my-lab-01/lab-29-async-demo/src/main/java/cn/iocoder/springboot/lab29/asynctask/Demo.java
@@ -0,0 +1,25 @@
+package cn.iocoder.springboot.lab29.asynctask;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Demo {
+
+  public static void main(String[] args) {
+    // 创建线程池。
+    ExecutorService executor = Executors.newFixedThreadPool(10);
+
+    // 提交任务到线程池中执行。
+    for (int i = 0; i < 20; i++) {
+      executor.submit(new Runnable() {
+        @Override
+        public void run() {
+          System.out.println(Thread.currentThread().toString());
+          System.out.println("异步调用测试");
+        }
+
+      });
+    }
+  }
+
+}
